9474b5d17693145c29920c84d920a3640f96c3af
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-03-31 20:20:56 +0600

    factories updated

--- TextFieldProgrammatically/Factories.swift
+++ TextFieldProgrammatically/Factories.swift
@@ -125,6 +125,19 @@ func makeSwitch(isOn: Bool) -> UISwitch {
     return theSwitch
 }
 
+func makeButton(withText text: String) -> UIButton {
+    let button = UIButton(type: .system)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setTitle(text, for: .normal)
+
+    var config = UIButton.Configuration.filled()
+    config.baseBackgroundColor = .systemBlue
+    config.cornerStyle = .capsule
+    config.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
+    button.configuration = config
+    return button
+}
+
 func makeProgressView() -> UIProgressView {
     let progressView = UIProgressView(progressViewStyle: .default)
     progressView.translatesAutoresizingMaskIntoConstraints = false
--- TextFieldProgrammatically/ViewController.swift
+++ TextFieldProgrammatically/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var myTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupViews()
+        setupLayouts()
     }
 
-
+    func setupViews() {
+        
+    }
+    
+    func setupLayouts() {
+        
+    }
 }
 
